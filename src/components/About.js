e115d83166f10a7e370b3fa51b92cec96e0b3cc4
Kelly Hogan <khogan974@gmail.com>
2019-04-01 23:41:16 -0400

    Added smooth scrolling

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,17 @@
 import React from "react"
 
-export default () => (
+import "./About.css"
 
-
-
+export default ({id}) => (
+  <div id={id}>
+  <h1>This is the about section</h1>
+  <p>This is the about section</p>
+  <p>This is the about section</p>
+  <p>This is the about section</p>
+  <p>This is the about section</p>
+  <p>This is the about section</p>
+  <p>This is the about section</p>
+  <p>This is the about section</p>
+  <p>This is the about section</p>
+  </div>
 );
---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import "./Contact.css"
+
+export default ({id}) => (
+  <div id={id}>
+  <h1>This is the contact section</h1>
+  <p>This is the contact section</p>
+  <p>This is the contact section</p>
+  </div>
+);
