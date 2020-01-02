e590821e84400afee634ca5005cdadc0600bb18d
Kelly Hogan <khogan974@gmail.com>
2020-01-02 00:02:49 -0500

    Added description and animation for About section

--- src/components/About.js
+++ src/components/About.js
@@ -4,14 +4,23 @@ import "./About.css"
 
 export default ({id}) => (
   <div id={id}>
-  <h1>This is the about section</h1>
-  <p>This is the about section</p>
-  <p>This is the about section</p>
-  <p>This is the about section</p>
-  <p>This is the about section</p>
-  <p>This is the about section</p>
-  <p>This is the about section</p>
-  <p>This is the about section</p>
-  <p>This is the about section</p>
+  <h1 
+    className="title"
+    data-sal="slide-right"
+    data-sal-easing="ease-in-cubic"
+    data-sal-delay="200"
+    data-sal-duration="400"
+  >
+   About 
+  </h1>
+  <p className="about-me">
+    I am a Software Developer for&nbsp;
+    <a href="https://www.jpmorganchase.com/" target="_blank">
+      JPMorgan Chase & Co.
+    </a>
+    &nbsp;in the New York Metropolitan Area.
+    <br/>
+    My main expertise is backend development but I also have a passion for mobile development and creating engaging user experiences.
+  </p>
   </div>
 );
