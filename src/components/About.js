40e4e9af9ee946ed5fd709208cc58aee67f2f4cc
Kelly Hogan <khogan974@gmail.com>
2020-01-02 23:32:45 -0500

    Changed formatting in about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,30 +1,29 @@
 import React from "react"
 
 import "./About.css"
-import ProfileImage from "./AboutImage"
+import profile from "../images/profile.png"
 
 export default ({id}) => (
-  <div id={id}>
-  <h1 
-    className="title"
-    data-sal="slide-right"
-    data-sal-easing="ease-in-cubic"
-    data-sal-delay="200"
-    data-sal-duration="400"
-  >
-   About 
-  </h1>
-  <div className="crop">
-    <ProfileImage />
-  </div>
-  <br />
-  <p className="about-me">
-    I am a Software Developer for&nbsp;
-    <a href="https://www.jpmorganchase.com/" target="_blank" rel="noopener noreferrer">
-      JPMorgan Chase & Co.
-    </a>
-    <br/>
-    My main expertise is backend development but I also have a passion for mobile development and creating engaging user experiences.
-  </p>
+  <div id={id} className="wrapper">
+      <img src={profile} className="profile" alt="Kelly Hogan"/>
+    <div className="about-wrapper">
+      <h1 
+        className="title"
+        data-sal="slide-right"
+        data-sal-easing="ease-in-cubic"
+        data-sal-delay="200"
+        data-sal-duration="400"
+      >
+       About 
+      </h1>
+        <p className="about-me">
+        I am a Software Developer for&nbsp;
+        <a href="https://www.jpmorganchase.com/" target="_blank" rel="noopener noreferrer">
+          JPMorgan Chase & Co.
+        </a>
+        <br/>
+        My main expertise is backend development but I also have a passion for mobile development and creating engaging user experiences.
+      </p>
+    </div>
   </div>
 );
