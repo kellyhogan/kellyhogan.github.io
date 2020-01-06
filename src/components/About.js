0c86d88384349dca0528866153dda7863d019b04
Kelly Hogan <khogan974@gmail.com>
2020-01-06 00:25:26 -0500

    Adjusted bar graph

--- src/components/About.js
+++ src/components/About.js
@@ -6,13 +6,7 @@ import profile from "../images/profile.png"
 export default ({id}) => (
   <div id={id} className="wrapper">
     <div className="about-wrapper">
-      <h1 
-        className="title"
-        data-sal="slide-right"
-        data-sal-easing="ease-in-cubic"
-        data-sal-delay="200"
-        data-sal-duration="400"
-      >
+      <h1 className="title">
        About 
       </h1>
     </div>
@@ -23,39 +17,81 @@ export default ({id}) => (
        <a href="https://www.jpmorganchase.com/" target="_blank" rel="noopener noreferrer">
          JPMorgan Chase & Co.
        </a>
-       &nbsp;My main expertise is backend development but I also have a passion for mobile development and creating engaging user experiences.
+       &nbsp;My main expertise is backend development but I also have a passion for creating engaging user experiences and an interest in mobile development.
       </p>
     </div>
 
     <div className="bar-graph-wrapper">
        <div className="bar">
           <span className="skill">Java</span>
-          <div className="bar-one generic-bar">
+          <div
+            className="generic-bar"
+            data-aos="anim-bar-one"
+            data-aos-duration="1200"
+            data-aos-delay="100"
+            data-aos-once="true"
+          >
+            <span className="percentage">90%</span>
           </div>
        </div>
        <div className="bar">
           <span className="skill">Python</span>
-          <div className="bar-two generic-bar">
+          <div
+            className="generic-bar"
+            data-aos="anim-bar-two"
+            data-aos-duration="1200"
+            data-aos-delay="150"
+            data-aos-once="true"
+          >
+            <span className="percentage">80%</span>
           </div>
        </div>
        <div className="bar">
           <span className="skill">JavaScript</span>
-          <div className="bar-three generic-bar">
+          <div
+            className="generic-bar"
+            data-aos="anim-bar-three"
+            data-aos-duration="1200"
+            data-aos-delay="150"
+            data-aos-once="true"
+          >
+            <span className="percentage">35%</span>
           </div>
        </div>
        <div className="bar">
           <span className="skill">React.js</span>
-          <div className="bar-four generic-bar">
+          <div
+            className="generic-bar"
+            data-aos="anim-bar-four"
+            data-aos-duration="1200"
+            data-aos-delay="150"
+            data-aos-once="true"
+          >
+            <span className="percentage">20%</span>
           </div>
        </div>
        <div className="bar">
           <span className="skill">SQL</span>
-          <div className="bar-five generic-bar">
+          <div
+            className="generic-bar"
+            data-aos="anim-bar-five"
+            data-aos-duration="1200"
+            data-aos-delay="160"
+            data-aos-once="true"
+          >
+            <span className="percentage">60%</span>
           </div>
        </div>
        <div className="bar">
           <span className="skill">Git</span>
-          <div className="bar-six generic-bar">
+          <div
+            className="generic-bar"
+            data-aos="anim-bar-six"
+            data-aos-duration="1200"
+            data-aos-delay="150"
+            data-aos-once="true"
+          >
+            <span className="percentage">72%</span>
           </div>
        </div>
     </div>
