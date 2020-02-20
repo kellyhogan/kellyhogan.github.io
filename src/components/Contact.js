a43681558e12fb5d5806ffe6bb0714a6cadb8448
Kelly Hogan <khogan974@gmail.com>
2020-02-19 22:48:13 -0500

    Fixed contact formatting

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,9 +3,27 @@ import React from "react"
 import "./Contact.css"
 
 export default ({id}) => (
-  <div id={id}>
-  <h1 className="contact-title">Contact</h1>
-  <p>This is the contact section</p>
-  <p>This is the contact section</p>
+  <div id={id} className="contact-wrapper">
+    <h1 className="contact-title">Contact</h1>
+    <form method="post" action="https://getform.io/f/9daacf01-bdae-4750-8cf9-5d8ecc307f91" className="contact-form">
+      <label>
+        Name
+        <input className="generic-contact" type="text" name="name"/>
+      </label>
+      <label>
+        Email
+        <input className="generic-contact" type="email" name="email"/>
+      </label>
+      <label>
+        Subject
+        <input className="generic-contact" type="text" name="subject"/>
+      </label>
+      <label>
+        Message
+        <textarea className="generic-contact" name="message" rows="5" />
+      </label>
+
+      <button className="contact-button" type="submit">Send</button>
+    </form>
   </div>
 );
