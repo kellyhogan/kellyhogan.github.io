1262687a0371501cad65234efa3101f538452f43
Kelly Hogan <khogan974@gmail.com>
2020-03-18 23:07:11 -0400

    Changed About Section layout

--- src/components/About.js
+++ src/components/About.js
@@ -1,27 +1,52 @@
 import React from "react"
 
 import "./About.css"
-import profile from "../images/profile.png"
-import github from "../images/github_black.svg"
-import linkedin from "../images/linkedin_black.svg"
+import profile from "../images/me.png"
+import github from "../images/github.svg"
+import linkedin from "../images/linkedin.svg"
 
 export default ({id}) => (
   <div id={id} className="about-wrapper">
+
     <div className="about-content-wrapper">
-      <h1 className="about-title">
-       About 
-      </h1>
+      <div className="about-image">
+        <img src={profile} className="profile" alt="Kelly Hogan"/>
+      </div>
+
+      <div className="about-me">
+        <h1 className="who-is-she">Who Is She?</h1>
+        <div className="who-am-i">
+          I am a Software Developer for&nbsp;
+          <a href="https://www.jpmorganchase.com/" target="_blank" rel="noopener noreferrer">
+            JPMorgan Chase & Co
+          </a>
+          . My main expertise is backend development but I also have a passion for creating engaging user experiences and an interest in mobile development.
+        </div> 
+
+        <div className="connect-wrapper">
+          <div className="about-me-graphics">
+            <a title="github" href="https://github.com/kellyhogan" target="_blank" rel="noopener noreferrer">
+              <img 
+                src={github} 
+                alt="GitHub" 
+                style={{ width: '3em', height: '3em'}}
+              />
+            </a>
+            <a title="linkedin" href="https://www.linkedin.com/in/kelly-hogan-184ba5105/" target="_blank" rel="noopener noreferrer">
+              <img 
+                src={linkedin} 
+                alt="Linkedin" 
+                style={{ width: '3em', height: '3em'}}
+              />
+            </a>
+          </div>
+        </div>
+
+
+      </div>
     </div>
-    <img src={profile} className="profile" alt="Kelly Hogan"/>
-    <div className="about-content-wrapper">
-      <p className="about-me">
-       I am a Software Developer for&nbsp;
-       <a href="https://www.jpmorganchase.com/" target="_blank" rel="noopener noreferrer">
-         JPMorgan Chase & Co.
-       </a>
-       &nbsp;My main expertise is backend development but I also have a passion for creating engaging user experiences and an interest in mobile development.
-      </p>
-    </div>
+
+    <h1 className="who-is-she about-me">Technical Skills</h1>
 
     <div className="bar-graph-wrapper">
        <div className="bar">
@@ -96,25 +121,6 @@ export default ({id}) => (
             <span className="percentage">72%</span>
           </div>
        </div>
-    </div>
-    <div className="connect-wrapper">
-      <span className="lets-connect">Let's connect</span>
-      <div className="about-me-graphics">
-        <a title="github" href="https://github.com/kellyhogan" target="_blank" rel="noopener noreferrer">
- 	  <img 
- 	    src={github} 
- 	    alt="GitHub" 
- 	    style={{ width: '3em', height: '3em'}}
- 	  />
- 	</a>
- 	<a title="linkedin" href="https://www.linkedin.com/in/kelly-hogan-184ba5105/" target="_blank" rel="noopener noreferrer">
- 	  <img 
- 	    src={linkedin} 
- 	    alt="Linkedin" 
- 	    style={{ width: '3em', height: '3em'}}
- 	  />
- 	</a>
-      </div>
     </div>
   </div>
 );
