a23ab7538c772b1e5b58ba49dc23f90b89c19928
Kelly Hogan <khogan974@gmail.com>
2020-04-20 19:46:51 -0400

    Updates

--- src/utils/typography.js
+++
@@ -1,9 +0,0 @@
-import Typography from "typography"
-import noriegaTheme from "typography-theme-noriega"
-noriegaTheme.baseFontSize = '16px'
-noriegaTheme.baseLineHeight = '1.5'
-
-const typography = new Typography(noriegaTheme)
-
-export const { scale, rhythm, options } = typography
-export default typography
